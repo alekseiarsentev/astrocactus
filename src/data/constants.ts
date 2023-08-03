1b0608e50ec324eb27ee8bd30fea8519eb31c5f6
Aleksei Arsentev <useruser.edd@icloud.com>
2023-08-03 17:28:00 +0300

    some changes

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,22 +1,22 @@
 export const MENU_LINKS = [
 	{
-		title: "Home",
+		title: "Главная",
 		path: "/",
 	},
 	{
-		title: "About",
+		title: "Про меня",
 		path: "/about/",
 	},
 	{
-		title: "Blog",
+		title: "Блог",
 		path: "/posts/",
 	},
 ];
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
-	discord: "https://github.com/chrismwilliams/astro-cactus",
+	github: "https://github.com/alekseiarsentev",
+	twitter: "https://eba.do/u/enoxix",
+	discord: "discordapp.com/users/1061378725909909554",
 	email: "me@example.com",
 };
